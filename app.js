7122894cf9920a57c6494efe1ccc91a0a26fcf7c
digbyk <digby@digby.net>
2016-02-27 14:04:10 +0000

    Initial push

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+require('dotenv').load({ silent: true });
+
+var path = require('path');
+var express = require('express');
+var helmet = require('helmet');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+var app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+app.use(session({
+	store: new RedisStore({ url: process.env.REDIS_URL }),
+	secret: 'scrt',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use('/', require('./routes'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function () {
+	console.log('info', 'Node app is running at localhost:' + app.get('port'));
+});
